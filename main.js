263eb26f60cb4adff67a60f04a90babf03e6b4be
gobanoff <a.kliosovas@yahoo.com>
2023-08-12 03:12:36 +0300

    m

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const result = document.getElementById("result");
+
 function getData() {
   fetch(`https://www.omdbapi.com/?apikey=91486a0e&s=marvel&page=${pageCount}`)
     .then((resp) => resp.json())
@@ -19,17 +19,19 @@ function getData() {
       result.innerHTML = html;
     });
 }
-let pageCount = 0;
-
+let pageCount = 1;
+const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const pages = document.getElementById("pages");
+const result = document.getElementById("result");
+
 function forw() {
   if (pageCount >= 30) next.setAttribute("disabled");
   getData();
   pageCount++;
   pages.innerText = pageCount;
 }
-const prev = document.getElementById("prev");
+
 function back() {
   if (pageCount <= 1) prev.setAttribute("disabled");
   getData();
